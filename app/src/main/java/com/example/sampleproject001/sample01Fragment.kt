5d6ecf68dc088965a124ee71fa93738210c2de96
unknown <junpei5000@outlook.jp>
2019-03-31 21:41:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sampleproject001/sample01Fragment.kt
@@ -0,0 +1,35 @@
+package com.example.sampleproject001
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class sample01Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample01, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_goto_sample02)
+            .setOnClickListener { v ->
+                Navigation.findNavController(v) // (1)
+                    .navigate(R.id.sample02Fragment) // (2)
+            }
+    }
+
+}
---
+++ app/src/main/java/com/example/sampleproject001/sample02Fragment.kt
@@ -0,0 +1,20 @@
+package com.example.sampleproject001
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class sample02Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample02, container, false)
+    }
+
+}
